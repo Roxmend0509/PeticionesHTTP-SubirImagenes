c90ffa13004bb0ab99ca731f34c3493841063c14
Roxana <rox050996@gmail.com>
2022-04-13 14:52:35 -0500

    finish CRUD

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import regeneratorRuntime from "regenerator-runtime";
-import { obtenerUsers } from "./js/http-provider";
+import * as CRUD from "./js/crud-provider";
+// import { obtenerUsers } from "./js/http-provider";
 //import { init } from "./js/chistes-page";
-import { init } from "./js/usuarios-page";
+// import { init } from "./js/usuarios-page";
 
-init();
+// init();
 
 //obtenerUsers().then(console.log)
+
+// CRUD.getUsuario(4).then(console.log)
+
+// CRUD.crearUsuario({
+//     name:'Rox',
+//     job:'Capintero'
+// }).then(console.log)
+
+// CRUD.updateUsuario(1,{
+//     name:'Rox',
+//     job:'Capintero'
+// }).then(console.log)
+
+CRUD.deleteUsuario(1).then(console.log)
---
+++ src/js/crud-provider.js
@@ -0,0 +1,51 @@
+import { async } from "regenerator-runtime";
+
+const urlCRUD = 'https://reqres.in/api/users';
+
+
+const getUsuario = async (id) =>{
+    const resp = await fetch(`${urlCRUD}/${id}`);
+
+    const {data} = await resp.json();
+
+    return data;
+}
+
+const crearUsuario = async (usuario) =>{
+    const resp = await fetch(urlCRUD,{
+        method:'POST',
+        body:JSON.stringify( usuario ),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+
+    return resp.json();
+}
+
+const updateUsuario = async (id,usuario) =>{
+    const resp = await fetch(`${urlCRUD}/${id}`,{
+        method:'PUT',
+        body:JSON.stringify( usuario ),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    });
+    
+    return resp.json();
+}
+
+const deleteUsuario = async (id) =>{
+    const resp = await fetch(`${urlCRUD}/${id}`,{
+        method:'DELETE'
+    });
+    
+    return (resp.ok) ? 'Borrado' : 'No Eliminado';
+}
+
+export{
+    getUsuario,
+    crearUsuario,
+    updateUsuario,
+    deleteUsuario
+}
